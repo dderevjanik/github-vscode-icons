24a829ea2ea2c2a8b7a99a9450f27561eadbb27a
daniel.derevjanik <daniel.derevjanik@gmail.com>
2017-10-10 20:16:56 +0200

    1.0.3 - Light icons are now showed properly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
-## [1.0.2] - 2017-XX-YY
+## [1.0.3] - 2017-10-10
+
+### Fixes
+
+- Showing light version of icons should work correctly
+
+## [1.0.2] - 2017-10-08
 
 ### Fixes
 
--- scripts/TransformIconDefinition.ts
+++ scripts/TransformIconDefinition.ts
@@ -57,7 +57,7 @@ const writeFile = (path: string, callback: () => any) => {
     writeFile('./src/json/folderNames.json', () => {
         const folderNames = Object.keys(icons.light.folderNames).reduce((acc, folderName) => ({
             ...acc,
-            [folderName]: icons.folderNames[folderName]
+            [folderName]: icons.light.folderNames[folderName]
         }), {});
         return folderNames;
     });
@@ -67,7 +67,7 @@ const writeFile = (path: string, callback: () => any) => {
     writeFile('./src/json/fileExtensions.json', () => {
         const fileExtensions = Object.keys(icons.light.fileExtensions).reduce((acc, fileExtension) => ({
             ...acc,
-            [fileExtension]: icons.fileExtensions[fileExtension]
+            [fileExtension]: icons.light.fileExtensions[fileExtension]
         }), {});
         return fileExtensions;
     });
@@ -76,7 +76,7 @@ const writeFile = (path: string, callback: () => any) => {
     writeFile('./src/json/fileNames.json', () => {
         const fileNames = Object.keys(icons.light.fileNames).reduce((acc, fileName) => ({
             ...acc,
-            [fileName]: icons.fileNames[fileName]
+            [fileName]: icons.light.fileNames[fileName]
         }), {});
         return fileNames;
     });
@@ -84,9 +84,26 @@ const writeFile = (path: string, callback: () => any) => {
     // LANGUAGES IDS
     writeFile('./src/json/languagesIds.json', () => {
         const languagesIds = Object.keys(langauges).reduce((acc, languageId) => {
-            const language = langauges[languageId];
+            const languageDef = langauges[languageId];
+            const languageExtension = languageDef.defaultExtension;
+            const iconFileName = icons.languageIds[languageId];
+
+            // sometimes, there's not icon for language, so skip it
+            if (iconFileName === undefined) {
+                return {
+                    ...acc
+                };
+            }
+
+            // light theme version should be
+            const withoutPrefix = iconFileName.slice(3); // remove prefix "_f_"
+            const lightIconFileName = `_f_light_${withoutPrefix}`;
+            const existsLightTheme = icons.iconDefinitions[lightIconFileName]; // try to find light theme of icon
+
             const langaugeIcon = {
-                [language.defaultExtension]: icons.languageIds[languageId]
+                [languageDef.defaultExtension]: existsLightTheme
+                    ? lightIconFileName
+                    : iconFileName
             };
             return {
                 ...acc,
