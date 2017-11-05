2dc7827df25c5ec4fade24107dc76188d7f5a581
dderevjanik <daniel.derevjanik@gmail.com>
2017-11-05 17:29:32 +0100

    Updated Webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,36 +1,9 @@
 const path = require('path');
 const Webpack = require('webpack');
+const baseConfig = require('./webpack.config.base');
 
-const config = {
-    entry: {
-        'content': './src/Index.ts'
-    },
-    target: 'web',
+const devConfig = {
     devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js',
-    },
-    devServer: {
-        contentBase: __dirname + '/public'
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        loaders: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader?configFile="tsconfig.json"',
-                options: {
-                    configFile: 'tsconfig.json'
-                }
-            }
-        ]
-    },
-    plugins: []
 };
 
-module.exports = config;
+module.exports = Object.assign(baseConfig, devConfig);
