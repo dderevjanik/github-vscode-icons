1f471c6056a1c419e4eb4f2e33b1fdc5911af8ad
dderevjanik <daniel.derevjanik@gmail.com>
2018-03-28 12:19:43 +0200

    Migrated to webpack 4

--- webpack.config.dev.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const Webpack = require('webpack');
-const baseConfig = require('./webpack.config.base');
-
-const devConfig = {
-    devtool: 'source-map',
-};
-
-module.exports = Object.assign(baseConfig, devConfig);
