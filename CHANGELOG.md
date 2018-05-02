54e26a93a9d6c4fe57a9292131edc621521a9292
daniel.derevjanik <daniel.derevjanik@gmail.com>
2018-05-02 23:05:11 +0200

    version 1.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
+## [1.0.11] - 2018-05-02
+
+### Fixes
+
+- Showing loading icon instead of file icon in Gitlab
+- Loading icon in Bitbucket
+
 ## [1.0.10] - 2018-03-23
 
 ### Changed
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const Webpack = require('webpack');
+
+const baseConfig = {
+  entry: {
+    content: './packages/content/Content.ts',
+    popup: './packages/popup/Popup.tsx',
+    background: './packages/background/Background.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: __dirname + '/public'
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader?configFile="tsconfig.json"',
+        options: {
+          configFile: 'tsconfig.json'
+        }
+      }
+    ]
+  },
+  plugins: []
+};
+
+module.exports = baseConfig;
