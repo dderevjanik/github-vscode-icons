7012b705ce9cb0f96201175442a70c45e69ce913
Kid <kidonng@gmail.com>
2020-08-08 16:08:12 +0800

    Use github-url-detection

--- packages/content/utils/PageDetect.ts
+++ packages/content/utils/PageDetect.ts
@@ -1,64 +1,7 @@
-// COPIED FROM https://github.com/sindresorhus/refined-github/blob/master/src/libs/page-detect.js
-
 import * as select from 'select-dom';
+import { isRepo, utils } from 'github-url-detection';
 
-export const isTrending = () => location.pathname.startsWith('/trending');
-
-export const getRepoPath = () => location.pathname.replace(/^\/[^/]+\/[^/]+/, '');
-
-export const isRepoTree = () => isRepo() && /\/tree\//.test(getRepoPath());
-
-export const isPR = () => isRepo() && /^\/pull\/\d+/.test(getRepoPath());
-
-export const isPRFiles = () => isRepo() && /^\/pull\/\d+\/files/.test(getRepoPath());
-
-export const isPRCommit = () => isRepo() && /^\/pull\/\d+\/commits\/[0-9a-f]{5,40}/.test(getRepoPath());
-
-export const isSingleCommit = () => isRepo() && /^\/commit\/[0-9a-f]{5,40}/.test(getRepoPath());
-
-export const isCompare = () => isRepo() && /^\/compare/.test(getRepoPath());
-
-export const hasDiff = () =>
-  isRepo() &&
-  (isSingleCommit() || isPRCommit() || isPRFiles() || isCompare() || (isPR() && select.exists('.diff-table')));
-
-export const isReleases = () => isRepo() && /^\/(releases|tags)/.test(getRepoPath());
-
-export const isNotifications = () => location.pathname.startsWith('/notifications');
-
-export const isRepoSettings = () => isRepo() && /^\/settings/.test(getRepoPath());
-
-export const getOwnerAndRepo = () => {
-  const [, ownerName, repoName] = location.pathname.split('/');
-
-  return {
-    ownerName,
-    repoName
-  };
-};
-
-export const hasCommentForm = () => select.exists('.js-previewable-comment-form');
-
-/**
- * Github related detections
- */
-// @todo Replace with DOM-based test because this is too generic #708
-export const isRepo = () => !isGist() && !isTrending() && /^\/[^/]+\/[^/]+/.test(location.pathname);
-
-export const isSingleFile = () => {
-  const { ownerName, repoName } = getOwnerAndRepo();
-  const blobPattern = new RegExp(`/${ownerName}/${repoName}/blob/`);
-  return isRepo() && blobPattern.test(location.href);
-};
-
-export const isCommit = () => isSingleCommit() || isPRCommit() || (isPRFiles() && select.exists('.full-commit'));
-
-export const isRepoRoot = () =>
-  isRepo() && /^(\/?|\/tree\/?)$/.test(getRepoPath()) && select.exists('.repository-content');
-
-export const isHistoryForFile = () => isRepo() && /^\/commits\/[0-9a-f]{5,40}\/.+/.test(getRepoPath());
-
-export const isGist = () => location.hostname.startsWith('gist.') || location.pathname.startsWith('gist/');
+export const isHistoryForFile = () => isRepo() && /^\/commits\/[0-9a-f]{5,40}\/.+/.test(utils.getRepoPath());
 
 /**
  * BitBucket related detections
