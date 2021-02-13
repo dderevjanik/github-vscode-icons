b5bcbcf652f71897e3c596c7c6f7c57b8d7dd5c2
dderevjanik <daniel.derevjanik@gmail.com>
2021-02-13 22:00:40 +0100

    Bump 1.0.23

--- packages/ManifestChrome.ts
+++ packages/ManifestChrome.ts
@@ -11,7 +11,7 @@ const manifest = {
     '48': 'icon48.png',
     '16': 'icon16.png'
   },
-  version: '1.22',
+  version: '1.23',
   author: 'Daniel Derevjanik <daniel.derevjanik@gmail.com>',
   offline_enabled: true,
   browser_action: {
