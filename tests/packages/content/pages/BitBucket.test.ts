48231a85e2b3b66d670a7a6725095587d1e9fcdf
s-weigand <s.weigand.phy@gmail.com>
2021-03-28 02:22:52 +0100

    Updated tests to wait longer for content render
    Since especially SourceForge and BitBucket take a lot of time to render (needed for the selector tests), there will be less 'random' failing when waiting longer for the proper render.

--- tests/packages/content/pages/BitBucket.test.ts
+++ tests/packages/content/pages/BitBucket.test.ts
@@ -1,24 +1,28 @@
-import { fetchRenderedDocument } from '../../../utils';
+import { fetchRenderedDocument,SITE_RETRIEVE_TIMEOUT } from '../../../utils';
+
 
 import { QUERY_FILE_TABLE_ROWS, QUERY_ICONS_TO_REPLACE } from '../../../../packages/content/pages/BitBucket';
 
 describe('Test Bitbucket queries', () => {
-  it('Repo root', async () => {
+  it('Repo root', async (done) => {
     const renderedDocument = await fetchRenderedDocument('https://bitbucket.org/pypa/distlib/src/master/');
     expect(renderedDocument.querySelectorAll(QUERY_FILE_TABLE_ROWS).length).not.toBe(0);
     expect(renderedDocument.querySelectorAll(QUERY_ICONS_TO_REPLACE).length).not.toBe(0);
-  });
-  it('Subfolder with filetable', async () => {
+    done();
+  },SITE_RETRIEVE_TIMEOUT);
+  it('Subfolder with filetable', async (done) => {
     const renderedDocument = await fetchRenderedDocument('https://bitbucket.org/pypa/distlib/src/master/tests/');
     expect(renderedDocument.querySelectorAll(QUERY_FILE_TABLE_ROWS).length).not.toBe(0);
     expect(renderedDocument.querySelectorAll(QUERY_ICONS_TO_REPLACE).length).not.toBe(0);
-  });
-  it('Open file', async () => {
+    done();
+  },SITE_RETRIEVE_TIMEOUT);
+  it('Open file', async (done) => {
     const renderedDocument = await fetchRenderedDocument(
       'https://bitbucket.org/pypa/distlib/src/master/README.rst',
       null
     );
     expect(renderedDocument.querySelectorAll(QUERY_FILE_TABLE_ROWS).length).toBe(0);
     expect(renderedDocument.querySelectorAll(QUERY_ICONS_TO_REPLACE).length).toBe(0);
-  });
+    done();
+  },SITE_RETRIEVE_TIMEOUT);
 });
--- tests/packages/content/pages/SourceForge.test.ts
+++ tests/packages/content/pages/SourceForge.test.ts
@@ -1,14 +1,16 @@
-import { fetchDocument } from '../../../utils';
+import { fetchDocument, SITE_RETRIEVE_TIMEOUT } from '../../../utils';
 
 import { QUERY_SOURCEFORGE_ITEMS } from '../../../../packages/content/pages/SourceForge';
 
 describe('Test SourceForge queries', () => {
-  it('Repo root', async () => {
+  it('Repo root', async (done) => {
     const fetchedDocument = await fetchDocument('https://sourceforge.net/projects/python-fire.mirror/files/');
     expect(fetchedDocument.querySelectorAll(QUERY_SOURCEFORGE_ITEMS).length).not.toBe(0);
-  });
-  it('Subfolder with filetable', async () => {
+    done();
+  }, SITE_RETRIEVE_TIMEOUT);
+  it('Subfolder with filetable', async (done) => {
     const fetchedDocument = await fetchDocument('https://sourceforge.net/projects/python-fire.mirror/files/v0.3.1/');
     expect(fetchedDocument.querySelectorAll(QUERY_SOURCEFORGE_ITEMS).length).not.toBe(0);
-  });
+    done();
+  }, SITE_RETRIEVE_TIMEOUT);
 });
