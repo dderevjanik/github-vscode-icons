67566ffcf580cf6271646d0fa87b67321d26c91e
Vitaly <vital2580@icloud.com>
2023-10-05 02:14:50 +0300

    make firefox manifest valid

--- packages/ManifesFirefox.ts
+++ packages/ManifesFirefox.ts
@@ -4,5 +4,8 @@ export const createFirefoxManifest = () => {
   const manifest = createChromeManifest();
   delete manifest['offline_enabled'];
   delete manifest['background']['persistent'];
+  manifest['background'].scripts = [manifest.background.service_worker];
+  manifest.background.page = 'background.html';
+  delete manifest.background.service_worker;
   return manifest;
 };
